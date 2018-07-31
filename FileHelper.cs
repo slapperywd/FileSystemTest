71fabd01b683aed61ee88f8966f96b652a44f1cb
Siarhei Mahlysh <maglish@mail.ru>
2018-07-31 16:38:14 +0300

    Initial commit

---
+++ FileHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace FileSystemTest
+{
+    public class FileHelper
+    {
+        /// <summary>
+        /// Waits until file is present in specific location
+        /// </summary>
+        /// <param name="filePath">path to file including file name</param>
+        /// <param name="secondsTimeout">timeout</param>
+        /// <returns>true if file is present</returns>
+        public static bool WaitForFileDownload(string filePath, int secondsTimeout = 20)
+        {
+            string fileName = Path.GetFileName(filePath);
+
+            var watcher = new FileSystemWatcher(Path.GetDirectoryName(filePath));
+            watcher.Filter = fileName;
+            watcher.NotifyFilter = NotifyFilters.FileName;
+            watcher.EnableRaisingEvents = true;
+
+            var result = watcher.WaitForChanged(
+                WatcherChangeTypes.Renamed, 
+                (int)TimeSpan.FromSeconds(secondsTimeout).TotalMilliseconds);
+   
+            if (result.TimedOut)
+            {
+                throw new Exception($"Download failed, timeout is {secondsTimeout} seconds, expected file name to be downloaded is {fileName}");
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\FilesTest\test.txt";
+            string moveToFolder = @"D:\FilesTest\FolderToMoveFilesTo\";
+            string fileName = "Harbinger F And G LLC v OM Group (UK) Ltd.doc";
+
+            string downloadsFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+            var fi = new FileInfo(filePath);
+            Console.WriteLine(fi.Exists);
+            Console.WriteLine(fi.Name);
+            Console.WriteLine(fi.CreationTime);
+     
+           // fi.CopyTo(moveToFolder + "test.txt");
+            Console.WriteLine(FileHelper.WaitForFileDownload(Path.Combine(downloadsFolder, fileName)));
+           // Console.ReadKey();
+        }
+
+        
+    }
+}
